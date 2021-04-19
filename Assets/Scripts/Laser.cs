f7b305cc5f5cfdbe9e6f1914f94d95be01f78145
zachlo89 <zachlo89@gmail.com>
2021-04-19 16:20:32 -0700

    enemies

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] private int _speed = 8;
+    
+    void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 8)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,36 +4,46 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float _speed;
     public float horizInput;
     public float vertInput;
+    [SerializeField] private float _speed;
+    [SerializeField] private GameObject _laserPrefab;
+    [SerializeField] private float _laserOffsetAmt = 0.9f;
+    [SerializeField] private float _fireRate = 0.15f;
+    [SerializeField] private float _canFire = 0.0f;
+    
     
     void Start()
     {
-        // tk curr pos and assign it as start pos
         // transform.position = new Vector3(0, 0, 0);
         transform.position = Vector3.zero;
     }
-    
+
     void Update() // called once per fr; 60 fr/sec
     {
         PlayerMovement();
-    }
-    
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            ShootLaser();
+        }
+    }     
+
     void PlayerMovement()
     {
         float horizInput = Input.GetAxisRaw("Horizontal");
         float vertInput = Input.GetAxisRaw("Vertical");
-        
+
         // transform.Translate(new Vector3(1, 0, 0) * _speed * realTime); // 1m to right at 60m/sec
         // transform.Translate(Vector3.right * horizInput * _speed *Time.deltaTime);
         // transform.Translate(Vector3.up * vertInput * _speed * Time.deltaTime);
 
         Vector3 direction = new Vector3(horizInput, vertInput, 0);
+        
         transform.Translate(direction * _speed * Time.deltaTime);
 
         transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.5f, 0), 0);
-        
+
         if (transform.position.x < -11.3f)
         {
             transform.position = new Vector3(11.3f, transform.position.y, 0);
@@ -42,5 +52,13 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(-11.3f, transform.position.y, 0);
         }
+    }
+
+    void ShootLaser()
+    {
+        Vector3 _laserOffset = new Vector3(0, _laserOffsetAmt, 0);
+
+        _canFire = Time.time + _fireRate;
+        Instantiate(_laserPrefab, transform.position + _laserOffset, Quaternion.identity);
     }
 }
