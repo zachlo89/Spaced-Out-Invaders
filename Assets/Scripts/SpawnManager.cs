afc8e245bf08ab260b5bd0a7741d2280c863e3da
zachlo89 <zachlo89@gmail.com>
2021-04-21 20:08:27 -0700

    spawn manager

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+    
+    // spawn game objects every 5 seconds
+    IEnumerator SpawnRoutine()
+    {
+        yield return new WaitForSeconds(5);
+    }
+}
