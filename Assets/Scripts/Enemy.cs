fc191a3263708edf9feb16335ba4468f5d6f9ed4
zachlo89 <zachlo89@gmail.com>
2021-05-18 17:29:47 -0700

    lives display

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81243e693429e9f66976143caa7bf9d006a297a287c4dbded19d9e0dee65719e
-size 1585
+oid sha256:8a2ea236e7ed276cd1f92c5780c05959b03996d27b7e67d621a66b4786304380
+size 1584
